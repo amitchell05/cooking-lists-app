9aad5ce2d79ed5ae54197886f8e8a6f19ea326b8
Angela Mitchell <anglarenmit@gmail.com>
2020-12-07 22:27:21 -0600

    Added navigation with event binding and ngIf

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'cooking-lists-app';
+  loadedFeature: string = 'recipe';
+
+  onNavigate(feature: string) {
+    this.loadedFeature = feature;
+  }
 }
