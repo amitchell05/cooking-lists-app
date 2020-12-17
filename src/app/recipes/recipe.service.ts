c07a7c177f1e91b34fe825149ad11c61110573b8
Angela Mitchell <anglarenmit@gmail.com>
2020-12-16 22:05:39 -0600

    Added ingredients to recipes; display recipe's ingredients in recipe-detail

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,16 +1,37 @@
-import { EventEmitter } from '@angular/core';
+import { EventEmitter, Injectable } from '@angular/core';
+import { Ingredient } from '../shared/ingredient.model';
 import { Recipe } from './recipe.model';
+
+@Injectable()
 
 export class RecipeService {
   recipeSelected = new EventEmitter<Recipe>();
 
   private recipes: Recipe[] = [
-    new Recipe('A Test Recipe', 'This is simply a test', 'https://storage.needpix.com/rsynced_images/food-1459693_1280.jpg'),
-    new Recipe('Another Test Recipe', 'This is simply a test', 'https://storage.needpix.com/rsynced_images/food-1459693_1280.jpg')
+    new Recipe(
+      'Tasty Schnitzel', 
+      'A super-tasty Schnitzel - just awesome!',
+      'https://upload.wikimedia.org/wikipedia/commons/7/72/Schnitzel.JPG',
+      [
+        new Ingredient('Meat', 1),
+        new Ingredient('French Fries', 20)
+      ]),
+    new Recipe(
+      'Big Fat Burger', 
+      'What else you need to say?', 
+      'https://tse4.mm.bing.net/th?id=OIP.qMAo5oMMltQub6eUWsInegHaFg&pid=Api',
+      [
+        new Ingredient('Buns', 2),
+        new Ingredient('Meat', 1)
+      ])
   ];
 
   getRecipes() {
       return this.recipes.slice();
+  }
+
+  addIngredientsToShoppingList(ingredients: Ingredient[]) {
+
   }
 
 }
