31bb0550ec661c765e4788525a4745b7949fdf84
Angela Mitchell <anglarenmit@gmail.com>
2020-12-22 21:55:22 -0600

    Removed click listeners and href attribute in header component

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,16 +1,10 @@
-import { Component, EventEmitter, Output} from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
-    selector: 'app-header',
-    templateUrl: './header.component.html',
-    styleUrls: ['./header.component.css']
-  })
-  
+  selector: "app-header",
+  templateUrl: "./header.component.html",
+  styleUrls: ["./header.component.css"],
+})
 export class HeaderComponent {
   collapsed: boolean = true;
-  @Output() featureSelected = new EventEmitter<string>();
-
-  onSelect(feature: string) {
-    this.featureSelected.emit(feature);
-  }
 }
