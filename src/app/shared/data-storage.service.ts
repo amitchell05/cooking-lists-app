4e86f667f13f28a0e9f0d1cc3e5d2a74501dc160
Angela Mitchell <anglarenmit@gmail.com>
2021-01-20 17:26:22 -0600

    Transformed response data for fetched recipes using pipe and map

--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { map } from 'rxjs/operators';
 
 import { Recipe } from '../recipes/recipe.model';
 import { RecipeService } from '../recipes/recipe.service';
@@ -26,6 +27,16 @@ export class DataStorageService {
     this.http
       .get<Recipe[]>(
         'https://ng-cooking-lists-app-default-rtdb.firebaseio.com/recipes.json'
+      )
+      .pipe(
+        map((recipes) => {
+          return recipes.map((recipe) => {
+            return {
+              ...recipe,
+              ingredients: recipe.ingredients ? recipe.ingredients : [],
+            };
+          });
+        })
       )
       .subscribe((recipes) => {
         this.recipeService.setRecipes(recipes);
