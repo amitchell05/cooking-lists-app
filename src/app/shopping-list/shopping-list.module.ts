a956b16b14bfb24a63d57067ab933dd9a8b56d03
Angela Mitchell <anglarenmit@gmail.com>
2021-02-05 16:32:22 -0600

    Added ShoppingList Module

---
+++ src/app/shopping-list/shopping-list.module.ts
@@ -0,0 +1,22 @@
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
+
+import { ShoppingListEditComponent } from './shopping-list-edit/shopping-list-edit.component';
+import { ShoppingListComponent } from './shopping-list.component';
+
+@NgModule({
+  declarations: [ShoppingListComponent, ShoppingListEditComponent],
+  imports: [
+    CommonModule,
+    FormsModule,
+    RouterModule.forChild([
+      {
+        path: 'shopping-list',
+        component: ShoppingListComponent,
+      },
+    ]),
+  ],
+})
+export class ShoppingListModule {}
